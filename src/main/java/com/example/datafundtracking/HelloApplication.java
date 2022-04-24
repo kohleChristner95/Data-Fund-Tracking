b2564eee0723be14b33df1972b1a9a8969b3c973
Kohle Christner <80296696+kohleChristner95@users.noreply.github.com>
2022-04-23 23:27:47 -0400

    Started adding changes to UI. Set height and width to 500, center screen on startup, set title to "Data Fund Tracking"

--- src/main/java/com/example/datafundtracking/HelloApplication.java
+++ src/main/java/com/example/datafundtracking/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.datafundtracking;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        stage.centerOnScreen();
+        stage.setTitle("Data Fund Tracking");
         stage.setScene(scene);
         stage.show();
     }
